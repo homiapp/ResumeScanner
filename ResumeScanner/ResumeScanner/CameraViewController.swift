50dda179e7c0494e051fda5e661e56ba8277fdc5
Jiatao Cheng <jiatao@jiataocheng.com>
2018-10-16 13:44:23 -0500

    added camera code

---
+++ ResumeScanner/ResumeScanner/CameraViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CameraViewController.swift
+//  ResumeScanner
+//
+//  Created by Jiatao Cheng on 10/16/18.
+//  Copyright © 2018 Homi, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+var captureSession: AVCaptureSession?
+var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+
+class CameraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+        } catch {
+            print(error)
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        previewView.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
