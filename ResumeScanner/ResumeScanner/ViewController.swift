9ef98ed65d2f7a1a32fe817f6861b8028d28442f
Jiatao Cheng <jiatao@jiataocheng.com>
2018-10-16 13:11:40 -0500

    added firebase auth

--- ResumeScanner/ResumeScanner/ViewController.swift
+++ ResumeScanner/ResumeScanner/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    @IBAction func login(_ sender: Any) {
+        let u_name = self.username.text as! String
+        let pass = self.password.text as! String
+        
+        Auth.auth().signIn(withEmail: u_name, password: pass) { (user, error) in
+            // ...
+            if ((user) != nil) {
+                //navigate to next page
+            } else {
+                //display error
+            }
+        }
+    }
+    
+    @IBAction func signup(_ sender: Any) {
+        let u_name = self.username.text as! String
+        let pass = self.password.text as! String
+        
+        Auth.auth().createUser(withEmail: u_name, password: pass) { (authResult, error) in
+            // ...
+            guard let user = authResult?.user else { return }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        Auth.auth().addStateDidChangeListener { (auth, user) in
+            // ...
+            //if auth then proceed
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
