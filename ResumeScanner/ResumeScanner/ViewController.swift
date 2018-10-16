91b514f93b80d6ca70c55397b344c1c3cb083c52
Jiatao Cheng <jiatao@jiataocheng.com>
2018-10-16 12:26:23 -0500

    Initial Commit

---
+++ ResumeScanner/ResumeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ResumeScanner
+//
+//  Created by Jiatao Cheng on 10/16/18.
+//  Copyright © 2018 Homi, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
